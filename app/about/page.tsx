a05bb12a91ec080921f5cdfab63435abe7fe49c2
Gon Skywalker <kornbetterclub@gmail.com>
2025-08-19 16:40:35 +0700

    Roitai Nexjs Basis

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const About = () => {
+  return (
+    <div>
+      
+      About
+    </div>
+  );
+};
+export default About;
---
+++ app/info/page.tsx
@@ -0,0 +1,11 @@
+import Counter from "@/components/Counter";
+
+const page = () => {
+  console.log('server korn counter')
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+};
+export default page;
